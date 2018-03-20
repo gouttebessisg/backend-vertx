ec6b557b968136dda41e701170dbb36c8d7ec4e6
Geoffroy Gouttebessis <geoffroy.gouttebessis@cgi.com>
2018-03-20 11:57:19 +0100

    Improve test for API

--- src/test/java/com/gouttebessisg/MyFirstVerticleTest.java
+++ src/test/java/com/gouttebessisg/MyFirstVerticleTest.java
@@ -2,6 +2,7 @@ package com.gouttebessisg;
 
 import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Vertx;
+import io.vertx.core.json.Json;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.unit.Async;
 import io.vertx.ext.unit.TestContext;
@@ -56,4 +57,27 @@ public class MyFirstVerticleTest {
       });
     });
   }
+
+  @Test
+public void checkThatWeCanAdd(TestContext context) {
+  Async async = context.async();
+  final String json = Json.encodePrettily(new Car("Car1", "White"));
+  final String length = Integer.toString(json.length());
+  vertx.createHttpClient().post(port, "localhost", "/api/cars")
+      .putHeader("content-type", "application/json")
+      .putHeader("content-length", length)
+      .handler(response -> {
+        context.assertEquals(response.statusCode(), 201);
+        context.assertTrue(response.headers().get("content-type").contains("application/json"));
+        response.bodyHandler(body -> {
+          final Car car = Json.decodeValue(body.toString(), Car.class);
+          context.assertEquals(car.getName(), "Car1");
+          context.assertEquals(car.getColor(), "White");
+          context.assertNotNull(car.getId());
+          async.complete();
+        });
+      })
+      .write(json)
+      .end();
+}
 }
